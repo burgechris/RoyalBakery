92128a1e39a92f9ad27cd57486773ae95a0ebc3d
Chris Burge <topher.burge@gmail.com>
2019-09-27 08:20:04 -0700

    greet the user when the application is run.

---
+++ program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace Bakery
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("What would you like?");
+    }
+  }
+}
