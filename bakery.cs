fcaf084522e0311fb4ff36dc198a955d030dca51
Chris Burge <topher.burge@gmail.com>
2019-09-27 08:44:30 -0700

    add constructor classes for bread and pastry.

--- bakery.cs
+++ bakery.cs
@@ -1,2 +1,19 @@
 using System;
 using System.Collections.Generic;
+
+namespace Bakery
+{
+  public class Bread
+  {
+    public string Name { get; set; } 
+    public int Price { get; set; }
+    public int Quantity { get; set; }
+  }
+
+  public class Pastry
+  {
+    public string Name { get; set; }
+    public int Price { get; set; }
+    public int Quantity { get; set; }
+  }
+}
--- program.cs
+++ program.cs
@@ -8,7 +8,11 @@ namespace Bakery
     public static void Main()
     {
       Console.WriteLine("Welcome to Pierre's Bakery!");
-      Console.WriteLine("What would you like?");
+      Console.WriteLine("Our menu consists of two items.");
+      Console.WriteLine("1. Bread: $5. Buy 2 loaves, get 1 free.");
+      Console.WriteLine("2. Pastry: $2 or 3 for $5.");
+      Console.WriteLine("What would you like? [Press 1 for Bread or 2 for Pastry.");
+      string userOrder = Console.ReadLine();
     }
   }
 }
