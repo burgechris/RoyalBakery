337b53d3e6f6d7fbe0bd1a4fff60306aa4e0c763
Chris Burge <topher.burge@gmail.com>
2019-09-27 14:13:32 -0700

    figure out a method for determining input type.

--- program.cs
+++ program.cs
@@ -12,15 +12,28 @@ namespace Bakery
       Console.WriteLine("---------------------------");
       Console.WriteLine("What is your name?");
       string custName = Console.ReadLine();
-      Console.WriteLine("Here is our menu:");
-      Console.WriteLine("1. Chocolate Babka: $5 for 1 loaf. Buy 2 loaves, get 1 free.");
-      Console.WriteLine("2. Black and White Cookie: $2 for 1 or 3 for $5.");
+      Console.WriteLine("Here is our bread menu:");
+      Console.WriteLine("1. Chocolate Babka: $5/loaf");
+      Console.WriteLine("2. Cinammon Babka: $5/loaf");
+      Console.WriteLine("3. Marble Rye: $5/loaf");
+      Console.WriteLine("All bread is Buy 2, get one free.");
+      Console.WriteLine("Please enter the quantity of breads you would like.");
+
+      string userInput = Console.ReadLine();
+      if(int.TryParse(userInput, out int breadQty))
+      {
+        // int breadQty = int.Parse(userInput);
+        Bread breadOrder = new Bread();
+        int bTotal = breadOrder.CalcBread(breadQty);
+      }
+      else
+      {
+        Console.WriteLine("Please enter a valid quantity.");
+      } 
+
+      Console.WriteLine("1. Black and White Cookie: $2 for 1 or 3 for $5.");
       Console.WriteLine("---------------------------");
       Console.WriteLine("How many loaves of marble rye would you like?");
-
-      int breadQty = int.Parse(Console.ReadLine());
-      Bread breadOrder = new Bread();
-      int bTotal = breadOrder.CalcBread(breadQty);
 
       Console.WriteLine("How many cookies would you like?");
 
